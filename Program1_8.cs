6779e66ab85ec9b952aac957be61823c79ef12de
AndrodDevelopers <SamusevMI@gmail.com>
2017-01-08 22:18:45 +0300

    blablabla

---
+++ Program1_8.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1L8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------PRIMER---------------------------");
+
+            Console.WriteLine("Stroki: ");
+            int n=int.Parse(Console.ReadLine());
+            Console.WriteLine("Stolbcy: ");
+            int m=int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; ++i, Console.WriteLine())
+            {
+                for (int j = 1; j <= m; ++j)
+                {
+                    Console.Write(" " + j);
+                }
+            }
+
+            Console.WriteLine("\n---------------------------#1.2---------------------------\n");
+
+            for (int i = 1; i <= 4; ++i, Console.WriteLine())
+            {
+                for (int j = 1; j <= 10; ++j)
+                {
+                    Console.Write(" " + j);
+                }
+            }
+            
+            Console.WriteLine("\n---------------------------#1.3---------------------------\n");
+
+            for (int i = 1; i <= 5; ++i, Console.WriteLine())
+            {
+                for (int j = -10; j <= 12; ++j)
+                {
+                    Console.Write(" " + j);
+                }
+            }
+            
+            Console.WriteLine("\n---------------------------#1.4---------------------------\n");
+            
+            int a = 41;
+            for (int i = 1; i <= 4; ++i, Console.WriteLine())
+            {
+                for (int j = 1; j <= 10; ++j)
+                {
+                    Console.Write(" " + a);
+                    a = a + 1;
+                }
+            }
+
+            Console.WriteLine("\n---------------------------#1.5---------------------------\n");
+            
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 0 + i; j++)
+                {
+                    Console.Write(" " + 5);
+                }
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine("\n---------------------------#1.6---------------------------\n");
+            
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 0; j <= 5 - i; j++)
+                {
+                    Console.Write(" " + 1);
+                }
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine("\n---------------------------#1.7---------------------------\n");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 0 + i; j++)
+                {
+                    Console.Write(" " + i);
+                }
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine("\n---------------------------#1.11---------------------------\n");
+
+            
+           for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 0 + i; j++)
+                {
+                    Console.Write(" " + j);
+                }
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine("\n---------------------------#2---------------------------\n");
+
+            Console.Write("Razmer doski = ");
+            int razmerPolya = int.Parse(Console.ReadLine());
+            if ((razmerPolya * razmerPolya) % 2 != 0)
+            {
+                Console.WriteLine("black: " + ((razmerPolya * razmerPolya) / 2 + 1));
+            }
+            else
+            {
+                Console.WriteLine("black: " + ((razmerPolya * razmerPolya) / 2));
+            }
+
+            Console.Write("Enter the number of cell:\nK = ");
+            int k = int.Parse(Console.ReadLine());
+            Console.Write("L = ");
+            int l = int.Parse(Console.ReadLine());
+            if ((n*n)%2==0)
+            {
+                Console.WriteLine("Back!");
+            }
+            else
+            {
+                Console.WriteLine("White!");
+            }
+
+            Console.Write("Enter the coordinates of first cell:\nK1 = ");
+            int k1 = int.Parse(Console.ReadLine());
+            Console.Write("L2 = ");
+            int l1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the coordinates of second cell:\nK1 = ");
+            int k2 = int.Parse(Console.ReadLine());
+            Console.Write("L2 = ");
+            int l2 = int.Parse(Console.ReadLine());
+            if ((((k1 + l1) % 2 == 0) && ((k2 + l2 ) % 2 == 0)) || (((k1 + l1) % 2 != 0) && ((k2 + l2) % 2 != 0)))
+            {
+                Console.WriteLine("Same!");
+            }
+            else
+            {
+                Console.WriteLine("Different!");
+            }
+
+            Console.WriteLine("\n---------------------------#3---------------------------\n");
+
+            Console.Write("day: ");
+            int day = int.Parse(Console.ReadLine());
+            Console.Write("month: ");
+            int month = int.Parse(Console.ReadLine());
+            Console.Write("year: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your date: {0:##}.{1:##}.{2:####}", day, month, year);
+            Console.WriteLine("1 - skolko proshlo\n2 - skolko ostalos\n3 - yesterday\n4 - tomorrow");
+            int q = int.Parse(Console.ReadLine());
+            int fullYear = 365;
+            switch (q)
+            {
+                case 1: Console.WriteLine("proshlo {0} dnej", day + month * 30); break;
+                case 2: Console.WriteLine("ostalos {0} dnej", fullYear - (day + month * 30)); break;
+                case 3: Console.WriteLine("vchera bylo {0:##}.{1:##}.{2:####}", day - 1, month, year); break;
+                case 4: Console.WriteLine("zavtra budet {0:##}.{1:##}.{2:####}", day + 1, month, year); break;
+            }
+
+        }
+    }
+
+}
---
+++ Program1_9.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1L9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter X: ");
+            double x = int.Parse(Console.ReadLine());
+            Console.Write("Enter Y: ");
+            double y = int.Parse(Console.ReadLine());
+            double z = min(3 * x, 2 * y) + min(x - y, x + y);
+            Console.WriteLine("Min = " + z);
+            
+            /////////// 1.1//////////
+            Console.WriteLine("---------#1.1---------");
+            Console.Write("Enter X: ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Y: ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Z: ");
+            int z1=int.Parse(Console.ReadLine());
+            Console.Write("Enter V: ");
+            int v1=int.Parse(Console.ReadLine());
+            int minxy = minOfTwo(x1, y1);
+            int minzv = minOfTwo(z1, v1);
+            Console.WriteLine("Min of XY : {0}; Min of ZV : {1}", minxy, minzv);
+             
+            /////////// 1.2//////////
+            Console.WriteLine("---------#1.2---------");
+            Console.Write("Enter X: ");
+            double x2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Y: ");
+            double y2 = int.Parse(Console.ReadLine());
+            double z2 = max(x, 2 * y - x) + max(5 * x + 3 * y, y);
+            Console.WriteLine("Max = " + z2);
+            
+            //////////// 1.3//////////
+            Console.WriteLine("---------#1.3---------");
+            Console.Write("f(x)=x^3-sinx\nEnter A : ");
+            double x3 = double.Parse(Console.ReadLine());
+            Console.Write("Enter B : ");
+            double x4 = double.Parse(Console.ReadLine());
+            double y3 = maxOfAAndB(x3);
+            double y4 = maxOfAAndB(x4);
+            if (y3 > y4)
+            {
+                Console.WriteLine("Max in A");
+            }
+            else
+            {
+                Console.WriteLine("Max in B");
+            }
+
+            ///////////// 1.4//////////
+            Console.WriteLine("---------#1.4---------");
+            Console.Write("f(x)=cos(2x)+sin(x-3)\nEnter A : ");
+            double a4 = getNumberFromKeyboard();
+            Console.Write("Enter B : ");
+            double b4 = getNumberFromKeyboard();
+            double y5 = function4(a4);
+            double y6 = function4(b4);
+            Console.WriteLine(bolsheMenshe(y5, y6));
+
+            ///////////// 2.2///////////
+            Console.WriteLine("---------#2.2---------");
+            Console.Write("A=");
+            double a = getNumberFromKeyboard();
+            Console.Write("B=");
+            double b = getNumberFromKeyboard();
+            Console.Write("h=");
+            double h = getNumberFromKeyboard();
+            for (double i = a; i <= b; i += h)
+            Console.WriteLine("f({0:f4})={1:f8}", i, f(i));
+            
+        }
+
+        static double f(double x)
+        {
+            double y;
+            if (Math.Abs(x) < 3)
+            {
+                y = Math.Sin(x);
+            }
+            else if (x< 9)
+            {
+                y = (Math.Sqrt(Math.Pow(x, 2) + 1)) / (Math.Sqrt(Math.Pow(x, 2) + 5));
+            }
+            else
+            {
+                y = Math.Sqrt(Math.Pow(x, 2) + 1) - Math.Sqrt(Math.Pow(x, 2) + 5);
+            }
+            return y;
+        }
+
+
+        static string bolsheMenshe(double a, double b)
+        {
+            string result;
+            if (a > b)
+            {
+                result = "Max in A";
+            }
+            else
+            {
+                result = "Max in B";
+            }
+            return result;
+        }
+
+        static double function4(double a)
+        {
+            return Math.Cos(2 * a) + Math.Sin(a - 3);
+        }
+
+        static double getNumberFromKeyboard()
+        {
+            return double.Parse(Console.ReadLine());
+        }
+
+        static double maxOfAAndB(double a)
+        {
+            double c = Math.Pow(a, 3) - Math.Sin(a);
+            return c;
+        }
+
+        static double max(double a1, double b1)
+        {
+            return (a1 > b1) ? a1 : b1;
+        }
+
+        static int minOfTwo(int a, int b)
+        {
+            return (a < b) ? a : b;
+        }
+
+        static double min(double a, double b)
+        {
+            return (a < b) ? a : b;
+            /*
+            double c;
+            if (a < b)
+            {
+                c = a;
+            }
+            else
+            {
+                c = b;
+            }
+            return c;
+             */
+
+        }
+    }
+}
